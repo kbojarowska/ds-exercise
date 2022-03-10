206fa864cd7fed58db2ac4ae4d0eba1accecd32b
kbojarowska <kbojarowska@wp.pl>
2022-03-10 14:52:41 +0100

    Created disabled button state

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div>
       <button className='button-s-b button-focused'>Create new</button>
-      <button className='button-s-n'>Create new</button>
+      <button disabled className='button-s-n'>Create new</button>
       <button className='button-s-s'>Create new</button>
     </div>
   );

3d8965bf5f3ba79265cb1fec32a98d08c1acef24
kbojarowska <kbojarowska@wp.pl>
2022-03-12 23:53:57 +0100

    Added color swatches to documentation, made loading screen, deleted loading-frame class from css file

--- README.md
+++ README.md
@@ -12,8 +12,6 @@ To see the result clone this repository and start app by `yarn start` command in
 
 ### Buttons
 
-The default button element is 32 points (pt) high and has got default, hovered, pressed and focused states.
-
 Button states:
 
 Default:\
@@ -52,11 +50,16 @@ Small             | 24 pt         | 12 pt
 
 The CSS file contains of color pallete.
 
-* classic blue tint 1
 * classic blue tint 2
+![classic blue tint 2 color](https://i.ibb.co/bKNgtgx/opera-zwu-MUpx-WEF.png)
+* classic blue tint 1
+![classic blue tint 1 color](https://i.ibb.co/qgvrTY7/opera-Vah-H3fzjjv.png)
 * classic blue
+![classic blue](https://i.ibb.co/xgD5Z7Z/opera-g-Bdiy-LWtf8.png)
 * classic blue shade 1
+![classic blue shade 1 color](https://i.ibb.co/k5Ryd0C/opera-c-Qn1-V8-GXa-T.png)
 * classic blue shade 2
+![classic blue shade 2 color](https://i.ibb.co/xChgq4S/obraz-2022-03-12-234156.png)
 
 To use colors from color pallete, use variable name in the style of element.
 
--- src/App.js
+++ src/App.js
@@ -4,31 +4,36 @@ import { useEffect, useState } from 'react'
 function App() {
   const [loading, setLoading] = useState(true)
 
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 3000)
-  }, [])
+  // useEffect(() => {
+  //   setTimeout(() => setLoading(false), 3000)
+  // }, [])
+
+  window.addEventListener('load', () => {
+    setLoading(!loading)
+  });
 
   return (
     <div>
       {loading ? 
-      <div className='loading-frame'>Loading...</div>
+      <div style={{'backgroundColor': 'white', 'position': 'absolute', 'top': '0px', 'right': '0px',
+      'bottom': '0px', 'left': '0px', 'textAlign': 'center', 'padding': '20% 0 20%', 'fontSize': '50pt'}}>Loading...</div>
       :
         <div>
           <h2>Color palette:</h2>
-          <div style={{'background-color': 'var(--classic-blue-t2)', 'padding':'10pt', 'max-width': '100pt'}}>Classic blue tint 2</div>
-          <div style={{'background-color': 'var(--classic-blue-t1)', 'padding':'10pt', 'max-width': '100pt'}}>Classic blue tint 1</div>
-          <div style={{'background-color': 'var(--classic-blue)', 'padding':'10pt', 'color': 'white', 'max-width': '100pt'}}>Classic blue</div>
-          <div style={{'background-color': 'var(--classic-blue-s1)', 'padding':'10pt', 'color': 'white', 'max-width': '100pt'}}>Classic blue shade 1</div>
-          <div style={{'background-color': 'var(--classic-blue-s2)', 'padding':'10pt', 'color': 'white', 'max-width': '100pt'}}>Classic blue shade 2</div>
+          <div style={{'backgroundColor': 'var(--classic-blue-t2)', 'padding':'10pt', 'maxWidth': '100pt'}}>Classic blue tint 2</div>
+          <div style={{'backgroundColor': 'var(--classic-blue-t1)', 'padding':'10pt', 'maxWidth': '100pt'}}>Classic blue tint 1</div>
+          <div style={{'backgroundColor': 'var(--classic-blue)', 'padding':'10pt', 'color': 'white', 'maxWidth': '100pt'}}>Classic blue</div>
+          <div style={{'backgroundColor': 'var(--classic-blue-s1)', 'padding':'10pt', 'color': 'white', 'maxWidth': '100pt'}}>Classic blue shade 1</div>
+          <div style={{'backgroundColor': 'var(--classic-blue-s2)', 'padding':'10pt', 'color': 'white', 'maxWidth': '100pt'}}>Classic blue shade 2</div>
 
           <h2>Normal buttons:</h2>
-          <button className='button-size-big' style={{'margin-right': '10pt'}}>Create new</button>
-          <button style={{'margin-right': '10pt'}}>Create new</button>
+          <button className='button-size-big' style={{'marginRight': '10pt'}}>Create new</button>
+          <button style={{'marginRight': '10pt'}}>Create new</button>
           <button className='button-size-small'>Create new</button>
           
           <h2>Disabled buttons:</h2>
-          <button disabled className='button-size-big' style={{'margin-right': '10pt'}}>Create new</button>
-          <button disabled style={{'margin-right': '10pt'}}>Create new</button>
+          <button disabled className='button-size-big' style={{'marginRight': '10pt'}}>Create new</button>
+          <button disabled style={{'marginRight': '10pt'}}>Create new</button>
           <button disabled className='button-size-small'>Create new</button>
         </div>
       }

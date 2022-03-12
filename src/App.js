10ef1de837c7c39f39bc857019b4fef7c07a4990
kbojarowska <kbojarowska@wp.pl>
2022-03-12 14:20:53 +0100

    Created loading screen

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import './styles/Buttons.css';
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 3000)
+  }, [])
+
   return (
     <div>
-      <h2>Color palette:</h2>
-      <div style={{'background-color': 'var(--classic-blue-t2)', 'padding':'10pt', 'max-width': '100pt'}}>Classic blue tint 2</div>
-      <div style={{'background-color': 'var(--classic-blue-t1)', 'padding':'10pt', 'max-width': '100pt'}}>Classic blue tint 1</div>
-      <div style={{'background-color': 'var(--classic-blue)', 'padding':'10pt', 'color': 'white', 'max-width': '100pt'}}>Classic blue</div>
-      <div style={{'background-color': 'var(--classic-blue-s1)', 'padding':'10pt', 'color': 'white', 'max-width': '100pt'}}>Classic blue shade 1</div>
-      <div style={{'background-color': 'var(--classic-blue-s2)', 'padding':'10pt', 'color': 'white', 'max-width': '100pt'}}>Classic blue shade 2</div>
+      {loading ? 
+      <div className='loading-frame'>Loading...</div>
+      :
+        <div>
+          <h2>Color palette:</h2>
+          <div style={{'background-color': 'var(--classic-blue-t2)', 'padding':'10pt', 'max-width': '100pt'}}>Classic blue tint 2</div>
+          <div style={{'background-color': 'var(--classic-blue-t1)', 'padding':'10pt', 'max-width': '100pt'}}>Classic blue tint 1</div>
+          <div style={{'background-color': 'var(--classic-blue)', 'padding':'10pt', 'color': 'white', 'max-width': '100pt'}}>Classic blue</div>
+          <div style={{'background-color': 'var(--classic-blue-s1)', 'padding':'10pt', 'color': 'white', 'max-width': '100pt'}}>Classic blue shade 1</div>
+          <div style={{'background-color': 'var(--classic-blue-s2)', 'padding':'10pt', 'color': 'white', 'max-width': '100pt'}}>Classic blue shade 2</div>
 
-      
-      <h2>Normal buttons:</h2>
-      <button className='button-size-big' style={{'margin-right': '10pt'}}>Create new</button>
-      <button style={{'margin-right': '10pt'}}>Create new</button>
-      <button className='button-size-small'>Create new</button>
-      
-      <h2>Disabled buttons:</h2>
-      <button disabled className='button-size-big' style={{'margin-right': '10pt'}}>Create new</button>
-      <button disabled style={{'margin-right': '10pt'}}>Create new</button>
-      <button disabled className='button-size-small'>Create new</button>
-    </div>
+          <h2>Normal buttons:</h2>
+          <button className='button-size-big' style={{'margin-right': '10pt'}}>Create new</button>
+          <button style={{'margin-right': '10pt'}}>Create new</button>
+          <button className='button-size-small'>Create new</button>
+          
+          <h2>Disabled buttons:</h2>
+          <button disabled className='button-size-big' style={{'margin-right': '10pt'}}>Create new</button>
+          <button disabled style={{'margin-right': '10pt'}}>Create new</button>
+          <button disabled className='button-size-small'>Create new</button>
+        </div>
+      }
+    </div>      
   );
 }
 
